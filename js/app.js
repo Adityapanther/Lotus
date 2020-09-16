ee3787c7b8201a8dc3f3d471f47f4341d6675cca
raj68518 <raj68518@gmail.com>
2020-09-16 20:20:27 +0530

    scroll sticky added

--- js/app.js
+++ js/app.js
@@ -1,12 +1,33 @@
+const header = document.getElementById("header");
+
+
 window.addEventListener('load', ()=>{
-    console.log("Hi..");
+    
     
 });
 
 
-document.body.onscroll( e=>{listenScroll()})
-function listenScroll(){
-    var data = window.scrollY;
-    console.log(data);
+window.addEventListener("scroll", (e)=>{
+   var data = scrollY
+   if(data > 30){
+       addStyleToHeader(true)
+       
+   }else if(data < 30){
+       addStyleToHeader(false)
+   }
+   
     
+})
+
+
+function addStyleToHeader(data){
+    if (data) {
+        header.classList.remove("border-bottom");
+        header.classList.add("header-sticky");
+        
+    }else {
+      header.classList.remove("header-sticky");
+      header.classList.add("border-bottom");
+    }
 }
+
